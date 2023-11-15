4cb468a9bf2394ec93b8fa0ba4e71a6e2c6ef753
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-15 19:17:14 +0100

    init

---
+++ gui.py
@@ -0,0 +1,34 @@
+import tkinter
+from tkinter import *
+import parser
+
+rows = []
+for i in range(5):
+    cols= []
+    for j in range(5):
+        e = Entry(relief=RIDGE)
+        e.grid(row=i, column = j, sticky=NSEW)
+        e.insert(END, '%d.%d' % (i, j))
+        cols.append(e)
+    rows.append(cols)
+
+master = tkinter.Tk()
+tkinter.Label(master, text="Number of columns").grid(row=0)
+tkinter.Label(master, text="Number of rows").grid(row=1)
+
+e_x = tkinter.Entry(master)
+e_y = tkinter.Entry(master)
+
+e_x.grid(row=0, column=1)
+e_y.grid(row=1, column=1)
+
+master.mainloop()
+
+def onPress():
+    for row in rows:
+        for col in cols:
+            print(col.get()),
+        print()
+
+Button(text='Fetch', command=onPress).grid()
+mainloop()
