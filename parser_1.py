e731004908cfedfb0976a174eedb46ceacc03a4b
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-17 01:49:20 +0100

    fix parsing and output

--- parser_1.py
+++ parser_1.py
@@ -10,53 +10,46 @@ from itertools import combinations
 # This file is for parsing the input files
 num = 1
 
-def get_game_field_size(num):
+def parse_input_field(num):
     # Gets game field from input .txt file and sets the size of the game.
 
     # Opens the file, reads everything. Prints a list. Since the first two numbers always indicate
     # x and y size, sets these manually. Creates a list with all the islands, their locations and
     # the number of bridges.
     file = f'Project_1/test{num}.txt'
+    contents = []
     with open(file) as t:  # to do: parse until space and do it again
-        size_info = t.readline()
-        size_info = size_info.split()
+        size_info = t.readline().split()
         x_size = int(size_info[0])
         y_size = int(size_info[1])
 
-    with open(file) as t:  # to do: parse until space and do it again
+        print(f"Dimensions are {x_size}, {y_size}\n")
         contents = t.readlines()
+        for line in contents:
+            line.strip()
 
     return x_size, y_size, contents
 
 
-def get_island_location(x_size, y_size, contents):
-    island_location = list()
-
-    for i in range(1, x_size + 1):
+# Initializes the field.
+def initialise_field(x_size, y_size, contents):
+    field = [["." for x in range(y_size)] for y in range(x_size)]  # Creates a 2D-Array all set with dots.
+    for i in range(x_size):
         for j in range(y_size):
             if contents[i][j] != '.':
-                island_location.append((i - 1, j, int(contents[i][j])))
+                field[i][j] = int(contents[i][j])
+            else:
+                field[i][j] = contents[i][j]
 
-    return island_location
-
-
-# Initializes the field.
-def field_initialization(x_size, y_size, island_location):
-    field = [["." for x in range(y_size)] for y in range(x_size)]  # Creates a 2D-Array all set with dots.
-
-    counter = 0
-    for _ in range(0, len(island_location)):  # Puts the islands on the field.
-        field[island_location[counter][0]][island_location[counter][1]] = island_location[counter][2]
-        counter += 1
-
-    for j in range(0, len(field)):
-        for i in range(0, len(field)):
-            print(field[i])
-
-        return field
+    for row in field:
+        for element in row:
+            print(element, end=' ')
+        print("\n")
+    return field
 
 
-def constraints(field, island_location):
+
+def build_constraints(field):
     class Node:
         def __init__(self, bridge_no, horizontal_bridge1=sympy.true, horizontal_bridge2=sympy.true, vertical_bridge1=sympy.true,
                      vertical_bridge2=sympy.true):
@@ -66,12 +59,12 @@ def constraints(field, island_location):
             self.v1 = vertical_bridge1
             self.v2 = vertical_bridge2
     f = CNF()
-    n = [[Node( 0) for x in range(len(field))] for y in range(len(field))]
+    n = [[Node(0) for x in range(len(field[0]))] for y in range(len(field))]
     vpool = IDPool()
     v = lambda i : vpool.id('{0}'.format(i))
     
     for i in range(0, len(field)):
-        for j in range(0, len(field)):
+        for j in range(0, len(field[0])):
             if field[i][j] == '.':
                 n[i][j] = Node(0, v(f'h_{i}_{j}'), v(f'dh_{i}_{j}'), v(f'v_{i}_{j}'), v(f'dv_{i}_{j}'))
             else:
@@ -81,7 +74,7 @@ def constraints(field, island_location):
 
     
     for i in range(0, len(field)):
-        for j in range(0, len(field)):
+        for j in range(0, len(field[0])):
             if n[i][j].val == 0:
                 f.extend([[-n[i][j].h1, -n[i][j].v1], [-n[i][j].h2, -n[i][j].v2],
                           [-n[i][j].h1, -n[i][j].v2],  [-n[i][j].h2, -n[i][j].v1]]
@@ -131,10 +124,9 @@ def main():
         num = int(sys.argv[1])
     
     print(f'Running solver on test {num}...\n')        
-    field_info = get_game_field_size(num)
-    island_info = get_island_location(field_info[0], field_info[1], field_info[2])
-    field = field_initialization(x_size=field_info[0], y_size=field_info[1], island_location=island_info)
-    constraints(field, island_info)
+    field_info = parse_input_field(num)
+    field = initialise_field(x_size=field_info[0], y_size=field_info[1], contents=field_info[2])
+    build_constraints(field)
 
 
 if __name__ == "__main__":
