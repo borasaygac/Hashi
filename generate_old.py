bfc6b15ab59be5b131da52d6d732daac4c205b54
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-17 21:49:17 +0100

    optimise by subsequent pair

--- generate_old.py
+++ generate_old.py
@@ -5,8 +5,6 @@ bridge_sum = {}
 
 for r in range(1, 5):  # We're interested in lengths 1 to 4
     for perm in combinations(arr, r):
-        s = set()
-        subsequent_pair = False
         count_hor = 0  # Counter for even numbers
         count_ver = 0
         total = 0
@@ -15,14 +13,11 @@ for r in range(1, 5):  # We're interested in lengths 1 to 4
                 count_ver += 1
             else:
                 count_hor += 1
-            if(((num-1) in s) & (num != 4) & (num % 2 == 1)):
-                subsequent_pair = True
-            else:
-                s.add(num)
-        if (count_hor > 2) | (count_ver > 2) | (subsequent_pair):
+        if count_hor > 2 | count_ver > 2:
             continue
         
         total = sum(2 if x % 2 == 0 else 1 for x in perm)
+
         if total not in bridge_sum:
              bridge_sum[total] = []
         bridge_sum[total].append(perm)
