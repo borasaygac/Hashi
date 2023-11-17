2334fbae2d033c992fbbee265ec4d2d0d4440583
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-17 20:27:12 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,7 +4,7 @@ arr = [0, 1, 2, 3, 4, 5, 6, 7]
 bridge_sum = {}
 
 for r in range(1, 5):  # We're interested in lengths 1 to 4
-     for perm in permutations(arr, r):
+    for perm in permutations(arr, r):
         count_even = 0  # Counter for even numbers
         count_odd = 0
         total = 0
