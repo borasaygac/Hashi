1b1e768fcd5974620fe4d76f6b12aff1ef0e903e
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-18 13:02:21 +0100

    intro aiger

--- parser_1.py
+++ parser_1.py
@@ -1,12 +1,13 @@
 import math
 import os
-import sympy
 import sys
-from sympy import *
 from pysat.formula import CNF, IDPool
 from pysat.solvers import Solver
 from itertools import combinations
 import aiger
+import time
+from aiger_cnf import aig2cnf
+from aiger import *
 
 # This file is for parsing the input files
 num = 1
@@ -52,8 +53,8 @@ def initialise_field(x_size, y_size, contents):
 
 def build_constraints(field):
     class Node:
-        def __init__(self, bridge_no, horizontal_bridge1=sympy.true, horizontal_bridge2=sympy.true, vertical_bridge1=sympy.true,
-                     vertical_bridge2=sympy.true):
+        def __init__(self, bridge_no, horizontal_bridge1=True, horizontal_bridge2=True, vertical_bridge1=True,
+                     vertical_bridge2=True):
             self.val = bridge_no
             self.h1 = horizontal_bridge1
             self.h2 = horizontal_bridge2
@@ -61,8 +62,8 @@ def build_constraints(field):
             self.v2 = vertical_bridge2
     f = CNF()
     n = [[Node(0) for x in range(len(field[0]))] for y in range(len(field))]
-    vpool = IDPool()
-    v = lambda i : vpool.id('{0}'.format(i))
+    pool = IDPool()
+    v = lambda i : pool.id('{0}'.format(i))
     
     for i in range(0, len(field)):
         for j in range(0, len(field[0])):
@@ -72,8 +73,8 @@ def build_constraints(field):
                 n[i][j] = Node(field[i][j], v(f'h_{i}_{j}'), v(f'dh_{i}_{j}'), v(f'v_{i}_{j}'), v(f'dv_{i}_{j}'))
                 f.extend([[n[i][j].h1],[n[i][j].v1],[n[i][j].h2],[n[i][j].v2] ])
                 
-    a = [[Node(0, Atom(n[x][y].h1), Atom(n[x][y].h2), Atom(n[x][y].v1), Atom(n[x][y].v2)) for y in range(len(field[0]))] for x in range(len(field))]
-    
+    #k = [[Node(0, Atom(n[x][y].h1), Atom(n[x][y].h2), Atom(n[x][y].v1), Atom(n[x][y].v2)) for y in range(len(field[0]))] for x in range(len(field))]
+    dnf = CNF()
     for i in range(0, len(field)):
         for j in range(0, len(field[0])):
             if n[i][j].val == 0:
@@ -87,16 +88,45 @@ def build_constraints(field):
                 if (j >= 1):
                     f.extend([[-n[i][j].h1, n[i][j-1].h1], [-n[i][j].h2, n[i][j-1].h2]])                    
                 if (j < len(field) - 1):
-                    f.extend([[-n[i][j].h1, n[i][j+1].h1], [-n[i][j].h2, n[i][j+1].h2]]) 
+                    f.extend([[-n[i][j].h1, n[i][j+1].h1], [-n[i][j].h2, n[i][j+1].h2]])
+            # else:
+                # a = aiger.atom(f'dh_{3}_{2}')
+                # b = aiger.atom(f'dh_{3}_{3}')
+                # expr = (a & b) 
+                
+                # cnf = CNF()
+                # cnf.from_aiger(expr.aig, vpool=pool)
+                # dnf.extend(cnf)
+                
+    print()              
     model = []
-    f.extend([[n[3][2].h2]])
+    p = aiger.atom(f'p')
+    q = aiger.atom(f'q')
+    r = aiger.atom(f'r')
+    s = aiger.atom(f's')
+    expr = ~((p | q) & r) | s
+    expr = p | q
+    cnf = CNF()
+    cnf.from_aiger(expr.aig)
+    dnf.extend(cnf)
+    dnf.append([70])
+    x = lambda x: pool.obj(x) if x > 0 else pool.obj(-x)
+    dnf = dnf.clauses
+    dnf = [[(int)(math.copysign(v(x(m).name), m)) if hasattr(x(m), 'name') else m for m in l] for l in dnf]
+    print('DNF:')
+    print(dnf)
+    print(['{0} <-> {1}'.format(v, cnf.vpool.obj(v)) for v in cnf.inps])
+    f.extend(dnf)
+    print('Formula:')
+    print(f.clauses)
+    
     with Solver(bootstrap_with=f) as s:
         s.solve()
         print("\nModel:")
         model = s.get_model()
     
-    res = list(map(lambda x: vpool.obj(x) if x > 0 else '~'+ vpool.obj(-x), model))
-    print(res)  
+    #res = list(map(lambda x: pool.obj(x) if x > 0 else '~'+ pool.obj(-x), model))
+    print(model)  
     output_folder = os.path.join(os.getcwd(), 'Solution')
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -105,7 +135,7 @@ def build_constraints(field):
         j = 0
         I = lambda i: (i // 4) // len(n[0])
         
-        for i in range(0, len(model), 4):
+        for i in range(0, len(n)*len(n[0])*4, 4):
             if n[I(i)][j].val != 0:
                 file.write(f'{n[I(i)][j].val}  ')
             else:
@@ -135,4 +165,6 @@ def main():
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    print("\n--- %s seconds ---" % (time.time() - start_time))
