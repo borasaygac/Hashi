9c914299d9e9f30fa0d93e349e0bd457810587ff
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-19 03:24:14 +0100

    not fixed

--- assets/suggestions_neighbour_set.py
+++ assets/suggestions_neighbour_set.py
@@ -4,7 +4,7 @@ grid = [
     [7, 0, 9, 10]
 ]
 
-non_zero_elements = {}  # Dictionary to store non-zero elements and their positions
+neighbours = {}  # Dictionary to store non-zero elements and their positions
 
 #WE PROBABLY WANT non_zero_elements to be a set since we can already access an element value through our field, so the mapping (i, j) -> value is unnecessary
 
@@ -12,45 +12,58 @@ non_zero_elements = {}  # Dictionary to store non-zero elements and their positi
 for i, row in enumerate(grid):
     for j, value in enumerate(row):
         if value != 0:
-            non_zero_elements[(i, j)] = value
-
+            neighbours[(i, j)] = []
+print(neighbours)
 # Function to find closest neighbors of a non-zero element
 def find_closest_neighbors(position):
     x, y = position
     closest_neighbors = []
-    
+    print(f"x,y: {x}, {y}")
     # Check right
     for dy in range(1, len(grid[0]) - y):
         neighbor = (x, y + dy)
-        if neighbor in non_zero_elements:
+        if neighbor in neighbours:
             closest_neighbors.append(neighbor)
             break
+        else:
+            if (dy == (len(grid[0]) - y - 1)) | (y == (len(grid[0]) - 1)):
+                closest_neighbors.append(None)
     
     # Check left
     for dy in range(1, y + 1):
         neighbor = (x, y - dy)
-        if neighbor in non_zero_elements:
+        if neighbor in neighbours:
             closest_neighbors.append(neighbor)
             break
+        else:
+            if (dy == y) | (y == 0):
+                closest_neighbors.append(None)
     
     # Check down
     for dx in range(1, len(grid) - x):
         neighbor = (x + dx, y)
-        if neighbor in non_zero_elements:
+        if neighbor in neighbours:
             closest_neighbors.append(neighbor)
             break
+        else:
+            if (dx == (len(grid) - x - 1)) | (x == (len(grid) - 1)) :
+                closest_neighbors.append(None)
+        
     
     # Check up
     for dx in range(1, x + 1):
         neighbor = (x - dx, y)
-        if neighbor in non_zero_elements:
+        if neighbor in neighbours:
             closest_neighbors.append(neighbor)
             break
+        else:
+            if (dx == x) | ( x == 0 ):
+                closest_neighbors.append(None)    
     
-    return closest_neighbors[:4]  # Return a maximum of 4 closest neighbors
+    return closest_neighbors  # Return a maximum of 4 closest neighbors
 
 # Iterate over non-zero elements and find their closest neighbors
-for position in non_zero_elements:
+for position in neighbours:
     closest_neighbors = find_closest_neighbors(position)
-    value = non_zero_elements[position]
-    print(f"Element {value} at {position} has closest neighbors at: {closest_neighbors}")
+    neighbours[position] = closest_neighbors
+    print(f"Element {position} has closest neighbors at: {neighbours[position]}")
