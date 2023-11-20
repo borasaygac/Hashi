627ef2b837efc4bdfaab367c48f63fb6dbebdd72
Tolga Engin <engint@cip.ifi.lmu.de>
2023-11-20 15:35:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
         num = int(sys.argv[1])
     print(f"Running solver on test {num}.......")
     field_info = parse_input_field(num)
-    field = initialise_field(field_info[0], field_info[1], field_info[2])
+    field, neighbours = initialise_field(field_info[0], field_info[1], field_info[2])
     nodes, vpool, formula = build_constraints(field)
     model = solve(vpool, formula)
     print_to_txt(nodes, model, num)
