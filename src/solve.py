c9c7af421de2eb4cc6bdc2e8e22fd24414412a8f
Lemar Abawi <abawi@cip.ifi.lmu.de>
2023-11-23 03:56:47 +0100

    rename

--- src/solve.py
+++ src/solve.py
@@ -6,30 +6,30 @@ def solve(vpool, f, neighbours, y):
 
     # interconnectivity: perform dfs on models to extract connected solution
 
-    def dfs(island, m, neighbours, counter, seen):
+    def dfs(island, model, neighbours, counter, seen):
         def pos(i, j): return 4*(y*i+j)
         i, j = island[0], island[1]
 
-        right = m[pos(i, j+1):(pos(i, j+1)+4)]
-        down = m[pos(i+1, j):(pos(i+1, j)+4)]
-        left = m[(pos(i, j-1)):(pos(i, j-1)+4)]
-        up = m[(pos(i-1, j)):(pos(i-1, j)+4)]
+        right = model[pos(i, j+1):(pos(i, j+1)+4)]
+        down = model[pos(i+1, j):(pos(i+1, j)+4)]
+        left = model[(pos(i, j-1)):(pos(i, j-1)+4)]
+        up = model[(pos(i-1, j)):(pos(i-1, j)+4)]
 
         if (right[0] > 0 or right[1] > 0) and (neighbours[(i, j)][0] not in seen):
             seen.add(neighbours[(i, j)][0])
-            counter += dfs(neighbours[(i, j)][0], m, neighbours, 1, seen)
+            counter += dfs(neighbours[(i, j)][0], model, neighbours, 1, seen)
 
         if (down[2] > 0 or down[3] > 0) and (neighbours[(i, j)][1] not in seen):
             seen.add(neighbours[(i, j)][1])
-            counter += dfs(neighbours[(i, j)][1], m, neighbours, 1, seen)
+            counter += dfs(neighbours[(i, j)][1], model, neighbours, 1, seen)
 
         if (left[0] > 0 or left[1] > 0) and (neighbours[(i, j)][2] not in seen):
             seen.add(neighbours[(i, j)][2])
-            counter += dfs(neighbours[(i, j)][2], m, neighbours, 1, seen)
+            counter += dfs(neighbours[(i, j)][2], model, neighbours, 1, seen)
 
         if (up[2] > 0 or up[3] > 0) and (neighbours[(i, j)][3] not in seen):
             seen.add(neighbours[(i, j)][3])
-            counter += dfs(neighbours[(i, j)][3], m, neighbours, 1, seen)
+            counter += dfs(neighbours[(i, j)][3], model, neighbours, 1, seen)
 
         return counter
 
